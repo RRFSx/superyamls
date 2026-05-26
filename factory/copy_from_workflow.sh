7dcf43d428cec7c249ac86c3dd617e85d5f6df3e
Guoqing Ge <guoqing.ge@noaa.gov>
2026-05-25 23:33:39 -0600

    major updates after renaming to yaml_factory (#4)
    * remove load_pyDAmonitor.sh and add ush/hook.sh
    * updates to match the latest rrfs-workflow YAMLs
    * rename rrfs_yaml to yaml_factory
    * create transition1
    * rename, convert_jcb-rdas_to_yamlfab.py
    * yaml_factory -> factory
    * update convert_jcb-rdas_to_yamlfab.py
    * rename, ush/guide.sh

---
+++ factory/copy_from_workflow.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -x
+\cp ../external/parm/jedivar.yaml .  # "\" is to bypass any cp alias
+\cp ../external/parm/getkf.yaml .
+\cp ../external/parm/hofx.yaml .
--- rrfs-yaml/copy_from_workflow.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -x
-\cp ../../jedivar.yaml .  # "\" is to bypass any cp alias
-\cp ../../getkf.yaml .
-\cp ../../hofx.yaml .
